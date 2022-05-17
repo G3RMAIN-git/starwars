f3c996e183318c2552a04d63b58af30fc61a2e67
Germain <germain.buch@gmail.com>
2022-05-17 12:08:12 +0200

    etoilemort atterire rebel/empire

--- src/be/technifutur/starwars/etoiledelamort.java
+++ src/be/technifutur/starwars/etoiledelamort.java
@@ -2,20 +2,27 @@ package be.technifutur.starwars;
 
 public class etoiledelamort {
 
-    public void attireVaisseau(VaisseauAncetre v){
-        v.atterir();
-        v.afficheCamp();
-        v.decoller();
+    public void attireVaisseau(VaisseauAncetre v) {
+        if (v instanceof VaisseauRebel) {
+            attireVaisseau((VaisseauRebel) v);
+        } else if (v instanceof VaisseauEmpire) {
+            attireVaisseau((VaisseauEmpire) v);
+        } else {
+            v.atterir();
+            v.afficheCamp();
+            v.decoller();
+        }
+
 
     }
 
-    public void attireVaisseau(VaisseauEmpire e){
+    public void attireVaisseau(VaisseauEmpire e) {
         e.atterir();
         e.afficheCamp();
         System.out.println("A ordres darklord");
     }
 
-    public void attireVaisseau(VaisseauRebel r){
+    public void attireVaisseau(VaisseauRebel r) {
         r.atterir();
         r.afficheCamp();
         System.out.println("Al'aide obiwan");
